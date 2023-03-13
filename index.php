9bdf7d62c66cd3c5b4c2857c9cfa7725b37df30a
rodriggodeveloper <rodriggo.eet@gmail.com>
2023-03-13 19:36:13 -0300

    teste do response

--- index.php
+++ index.php
@@ -3,6 +3,10 @@
     require __DIR__.'/vendor/autoload.php';
 
     use \App\Controller\Pages\Home;
-    $obResponse = new \App\Http\Response(200 ,'hello word');
-    
+    $obResponse = new \App\Http\Response(404 ,'hello world');
+
+    $obResponse -> sendResponse();
+
+
+    exit;
     echo Home::getHome();
