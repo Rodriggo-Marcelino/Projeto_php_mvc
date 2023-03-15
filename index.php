b798c402cd5b87756a82737d7025f5787a1e6ca5
rodriggodeveloper <rodriggo.eet@gmail.com>
2023-03-14 23:36:55 -0300

    implementações de debuger no index

--- index.php
+++ index.php
@@ -3,15 +3,19 @@
     require __DIR__.'/vendor/autoload.php';
 
 	use \App\Http\Router;
+	use \App\Http\Response;
     use \App\Controller\Pages\Home;
 
 	define('URL','http://localhost/mvc');
 	
 	$obRouter  = new router(URL);
-	
-	echo "<pre>";
-	print_r($obRouter);
-	echo "</pre>";
 
-	exit;
-    echo Home::getHome();
+	//ROTA HOME
+	$obRouter->get('/',[
+		function (){
+			return new Response(200,Home::getHome());
+		}
+	]);
+
+	
+	
