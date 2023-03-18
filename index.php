316fde70650063bafcadf6299b3f811e53dc4414
rodriggodeveloper <rodriggo.eet@gmail.com>
2023-03-18 02:21:16 -0300

    defenição de url

--- index.php
+++ index.php
@@ -3,10 +3,17 @@
     require __DIR__.'/vendor/autoload.php';
 
 	use \App\Http\Router;
+	use \App\Utils\View;
     
 
-	define('URL','http://localhost/mvc');
+	define('URL','http://localhost:8000/mvc');
+
+	//DEFINE O VALOR PADRÃO DAS VARIAVEIS  
+	View::init([
+		'URL' => URL
+	]);
 	
+	//INICIA O ROUTER 
 	$obRouter = new Router(URL);
 
 	//inclui as rotas de paginas 
